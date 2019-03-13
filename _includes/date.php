eb46338b748d07a7e87eadae9c0ff5786a6418f7
Goldberg <joshgoldberg@quickenloans.com>
2019-03-13 14:35:30 -0400

    Create date.php

---
+++ _includes/date.php
@@ -0,0 +1,13 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+ini_set('allow_url_fopen', 1);
+$tz = 'Europe/Amsterdam';
+$timestamp = time();
+$dt = new DateTime("now", new DateTimeZone($tz)); //first argument "must" be a string
+$dt->setTimestamp($timestamp); //adjust the object to correct timestamp
+$json = file_get_contents('http://localhost:5000/api/time');
+$obj = json_decode($json);
+$ICDate = $obj->iDay . "-" . $obj->iMonth . "-" . $obj->iYear . $obj->iYearAfter;
+$ICTime =  $dt->format('H:i:s') . "\n";
+?>
